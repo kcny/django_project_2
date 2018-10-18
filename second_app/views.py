bc98b84d11df5548412c3115bd1b75fa9d5cd758
Kudzai Nyandoro <kudzain@gmail.com>
2018-10-18 06:51:31 -0700

    Add templates.

--- second_app/views.py
+++ second_app/views.py
@@ -5,3 +5,7 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("<em>My Second App</em>")
+
+def help(request):
+    helpdict = {'help_insert':'HELP PAGE'}
+    return render(request,'second_app/help.html',context=helpdict)
--- second_project/urls.py
+++ second_project/urls.py
@@ -16,8 +16,12 @@ Including another URLconf
 from second_app import views
 from django.conf.urls import url
 from django.contrib import admin
+from django.conf.urls import include
+from second_app import views
+
 
 urlpatterns = [
     url(r'^$',views.index,name='index'),
+    url(r'^help',include('second_app.urls')),
     url(r'^admin/', admin.site.urls),
 ]
